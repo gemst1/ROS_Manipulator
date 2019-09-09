6e04f7946356a3351114357b62628afb44644192
Wonseok Jeon <wsjeonno@yonsei.ac.kr>
2019-09-09 12:20:27 +0900

    Create trajectory.py

---
+++ mservo_mani/scripts/trajectory.py
@@ -0,0 +1,31 @@
+import csv
+import numpy as np
+
+dir = '/Volumes/MSERVO_Cloud/2.연구실자료/1.개인자료/노우석/data2.txt'
+
+file = open(dir, 'r', encoding='utf-8')
+reader = csv.reader(file)
+
+data = []
+
+for line in reader:
+    data.append(line)
+
+traj_data = np.array(data)
+traj_len = traj_data.shape[0]
+
+def trajectory():
+    pub = rospy.Publisher('traj', traj, queue_size=1)
+    rospy.init_node('agent', anonymous=True)
+    rate = rospy.Rate(25)
+    i = 0
+    while not rospy.is_shutdown():
+        pub.publish(traj_data[i,:])
+        if i == traj_len:
+            break
+        else:
+            i = i + 1
+        rate.sleep()
+
+if __name__ == '__main__':
+    trajectory()
